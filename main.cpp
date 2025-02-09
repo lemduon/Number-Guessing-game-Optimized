ca5c27a62e04d38c08199f8b42798fdca5eed5c1
lemduon <lemduon@gmail.com>
2025-02-09 22:35:07 +0700

    Update main.cpp
    Patching Unexpected Results

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <limits>
 
 using std::cout;
 using std::cin;
@@ -10,22 +11,29 @@ int main() {
     int number = (rand() % 20) + 1;
     int ans;
     int tries = 0;
+
     while (true) {
         cout << "Enter your guess: ";
         cin >> ans;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Invalid input! Please enter a number." << endl;
+            continue;
+        }
+
+        tries += 1; // Increment tries only when input is valid
+
         if (ans == number) {
             cout << "Correct!" << endl;
-            tries += 1;
             break;
         } else if (ans < number) {
             cout << "Too low! Try Again!" << endl;
-            tries += 1;
-        } else if (ans > number) {
-            cout << "Too high! Try again!" << endl;
-            tries += 1;
         } else {
-            cout << "Invalid input!" << endl;
+            cout << "Too high! Try again!" << endl;
         }
     }
-    cout << "You got it in " << tries << " try/tries.";
+
+    cout << "You got it in " << tries << " try/tries." << endl;
 }
